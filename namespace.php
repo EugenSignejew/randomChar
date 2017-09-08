c0c646bf64b1d6f7cb2f8ebbce1a0331a90bfb4e
Unknown <eugen.signejew@brainformatik.com>
2017-09-08 12:23:27 +0200

    op

--- namespace.php
+++ namespace.php
@@ -1,64 +1,75 @@
 <?php
 
-namespace test;
+$pdo = new PDO('mysql:host=localhost;dbname=test', 'root', '');
+$pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$sql = "SELECT * FROM users ORDER BY email";
+foreach ($pdo->query($sql) as $row) {
+    echo $row["email"] . "<br>";
+}
+
+$statement = $pdo->prepare("DELETE FROM users WHERE id = ?");
+$statement->execute(array(5));
 
 
-class MyClass
-{
+echo "<br><br>";
+class MyClass {
     public $prop1 = "I'm a class property!";
 
     public static $count = 0;
 
-    public function __construct()
-    {
+    public function __construct() {
         echo 'The class "', __CLASS__, '" was initiated!<br />';
     }
 
-    public function __destruct()
-    {
+    public function __destruct() {
         echo 'The class "', __CLASS__, '" was destroyed.<br />';
     }
 
-    public function __toString()
-    {
+    public function __toString() {
         echo "Using the toString method: ";
+
         return $this->getProperty();
     }
 
-    public function setProperty($newval)
-    {
+    public function setProperty($newval) {
         $this->prop1 = $newval;
     }
 
-    private function getProperty()
-    {
+    private function getProperty() {
         return $this->prop1 . "<br />";
     }
 
-    public static function plusOne()
-    {
+    public static function plusOne() {
         return "The count is " . ++self::$count . ".<br />";
     }
 }
 
-class MyOtherClass extends MyClass
-{
-    public function __construct()
-    {
+class MyOtherClass extends MyClass {
+    public function __construct() {
         parent::__construct();
         echo "A new constructor in " . __CLASS__ . ".<br />
 verbinde mich mit der datenbank ";
     }
 
-    public function newMethod()
-    {
+    public function newMethod() {
         echo "From a new method in " . __CLASS__ . ".<br />";
     }
 
 }
 
-do
-{
+echo "<form method='post' action=''><button>count up to</button><input type='text' name='number'>
+</form><br>";
+
+
+if (empty($_POST["number"])) {
+    $number = 1;
+}else{
+    $number = $_POST["number"];
+}
+
+do {
     // Call plusOne without instantiating MyClass
     echo MyClass::plusOne();
-} while ( MyClass::$count < 10 );
+} while (MyClass::$count < $number);
