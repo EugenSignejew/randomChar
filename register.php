2533c3a2fb03725b341e573b2bc69ed31fb094b9
Eugen Signejew <eugen.signejew@brainformatik.com>
2017-09-11 14:44:27 +0200

    hügel

--- register.php
+++ register.php
@@ -74,7 +74,7 @@ if ($showFormular) {
         </div>
     </div>
     <?php
-} //Ende von if($showFormular) ende von apples
+} //Ende von if($showFormular) ende von apples and apples
 ?>
 
 </body>
