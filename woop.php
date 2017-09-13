286d5b908f21354cee7c323a8f5b3e10d8d5cb06
Eugen Signejew <eugen.signejew@brainformatik.com>
2017-09-13 16:43:57 +0200

    more of the woop

--- woop.php
+++ woop.php
@@ -1,17 +1,17 @@
 <?php
 echo "woop.php<br><br>";
 //w o o p . p h p
-//o
-//o
-//p
-//.
-//p
-//h
-//p
+//o o
+//o   o
+//p     p
+//.       .
+//p         p
+//h           h
+//p             p
 
 $pdo = new PDO("mysql:host=localhost;dbname=test", "root", "");
 
 $sql="SELECT * FROM `woop.php` ORDER BY rand();";
 foreach($pdo->query($sql) as $row){
-    echo "gotta get money<br>".$row["id"]." ".$row["woop1.php"].", ".$row["woop2.php"].", ".$row["woop3.php"]."<br><br> ";
+    echo "gotta get woop.php<br>".$row["id"]." ".$row["woop1.php"].", ".$row["woop2.php"].", ".$row["woop3.php"]."<br><br> ";
 }
