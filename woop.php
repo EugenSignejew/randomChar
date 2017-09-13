f516f130dfad3fbfe97b15208026de3f2aa25f9b
Eugen Signejew <eugen.signejew@brainformatik.com>
2017-09-13 14:34:58 +0200

    woop.php

--- woop.php
+++ woop.php
@@ -1,5 +1,5 @@
 <?php
-echo "woop.php";
+echo "woop.php<br><br>";
 //w o o p . p h p
 //o
 //o
@@ -8,3 +8,10 @@ echo "woop.php";
 //p
 //h
 //p
+
+$pdo = new PDO("mysql:host=localhost;dbname=test", "root", "");
+
+$sql="SELECT * FROM `woop.php`";
+foreach($pdo->query($sql) as $row){
+    echo $row["id"]." ".$row["woop1.php"].", ".$row["woop2.php"].", ".$row["woop3.php"]."<br><br>";
+}
